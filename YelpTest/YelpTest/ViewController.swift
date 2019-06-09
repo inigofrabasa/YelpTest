f1a03005f3190068896317b43b686caf47abc268
Iñigo Flores <inigo.f.rabasa@gmail.com>
2019-06-09 10:18:57 -0500

    New Project
    Project files created

---
+++ YelpTest/YelpTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YelpTest
+//
+//  Created by Emmanuel Casañas Cruz on 6/9/19.
+//  Copyright © 2019 Iñigo Flores Rabasa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
